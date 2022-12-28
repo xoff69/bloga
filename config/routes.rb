30da37903797b05c4fd9d2900f3114d955196354
xoff69 <xoff.pichon@gmail.com>
2022-12-28 11:49:16 -0500

    crud cru ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/articles", to: "articles#index"
+  
+
+  root "articles#index"  #car root est un point d acces particuler
+# on peut mettre resources :articles a la place de tout ca
+  resources :articles
+  # get "/articles/new", to: "articles#new"
+  
+  # get "/articles", to: "articles#index"
+  # post "/articles", to: "articles#create"
+
+  # get "/articles/:id/edit", to: "articles#edit", as: 'article_edit'
+  # patch "/articles/:id", to: "articles#update"  # put n a pas l air ok
+
+  # delete "/articles/:id", to: "articles#destroy"
+
+  # get "/articles/:id", to: "articles#show", as: :article
 end
